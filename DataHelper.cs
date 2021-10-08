7827f7ef0ca7546c721c774581b96b901720c112
jnsilva1 <josenilo.cp@gmail.com>
2021-10-07 21:43:42 -0300

    Adicionado DataHelper para facilitar a execução de códigos junto ao banco de dados (CRUD)

--- DataHelper.cs
+++ DataHelper.cs
@@ -6,5 +6,13 @@ namespace CadastroPessoaFisica
 {
     internal class DataHelper
     {
+        private static string ConnectionString;
+        /// <summary>
+        /// Define a cadeia de conexão com o Banco
+        /// </summary>
+        /// <param name="connectionString">Cadeia de conexão com o banco de dados</param>
+        static void SetConnectionString(string connectionString)
+            => connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+
     }
 }
